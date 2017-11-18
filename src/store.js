11d2c82d60352ea79dfb771b261d93880d18c4a5
Gareth Dsouza <gdsouza@usc.edu>
2017-11-18 13:12:08 -0800

    added redux dev tools suppport

--- src/store.js
+++ src/store.js
@@ -1,10 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import ReduxPromise from 'redux-promise';
 
 import rootReducer from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
-
+const createStoreWithMiddleware = composeWithDevTools(applyMiddleware(ReduxPromise))(createStore);
 const store = createStoreWithMiddleware(rootReducer);
 
 export default store;
+
+
+
+
+
+// const store = createStore(reducer, composeWithDevTools(
+//   applyMiddleware(...middleware),
+//   // other store enhancers if any
+// ));
